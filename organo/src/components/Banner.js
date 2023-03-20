ebfde2b15404e69f9b054d7d76427ff408f48caf
Rodrigo de Souza <rsouza01@gmail.com>
2023-03-20 09:07:37 +0000

    First commit

---
+++ organo/src/components/Banner.js
@@ -0,0 +1,10 @@
+import "./Banner.css";
+function Banner() {
+  return (
+    <header className="banner">
+      <img src="/images/banner.png" alt="Main banner" />
+    </header>
+  );
+}
+
+export default Banner;
